d2fedd625075b9f57ff82e1eabaa2e3d15c4422a
michael2112 <mike@senseoflifesolutions.com>
2019-08-15 17:30:08 -0500

    latest app changes

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use the decorator pattern to
 # link the view function to a url
 @app.route("/")
@@ -14,6 +17,37 @@ app = Flask(__name__)
 # define the view using a function, which returns a string
 def hello_world():
     return "Hello, World!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+# integer route
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+# float route
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
+# name route
+@app.route("/name/<name>")
+def name_type(name):
+    if name.lower() == "kate":
+        return "Hello {}".format(name)
+    else:
+        return "Not Found", 404
 
 # start the development server using the run() method
 if __name__ == "__main__":
